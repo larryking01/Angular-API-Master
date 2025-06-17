e965986fb52ca67c97d807ea22cdb83031936b5d
larry <larryking8118@gmail.com>
2025-06-17 15:26:46 +0000

    initial project setup done

---
+++ src/app/components/list-posts/list-posts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-posts',
+  imports: [],
+  templateUrl: './list-posts.html',
+  styleUrl: './list-posts.scss'
+})
+export class ListPosts {
+
+}
