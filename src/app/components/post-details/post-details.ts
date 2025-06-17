afffbec18864be4b31d153c387870f4279b79966
larry <larryking8118@gmail.com>
2025-06-17 20:46:55 +0000

    feature: implemented CRUD operations

--- src/app/components/post-details/post-details.ts
+++ src/app/components/post-details/post-details.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api-service';
+import { PostInterface } from '../../../shared/model';
+
 
 @Component({
   selector: 'app-post-details',
@@ -6,6 +10,41 @@ import { Component } from '@angular/core';
   templateUrl: './post-details.html',
   styleUrl: './post-details.scss'
 })
-export class PostDetails {
+export class PostDetails implements OnInit {
+
+  activatedRoute = inject(ActivatedRoute);
+
+  apiService = inject(ApiService);
+
+  postComments: any = [];
+
+  postDetail: PostInterface | null = null;
+
+
+  ngOnInit(): void {
+    let selectedPostID = this.activatedRoute.snapshot.paramMap.get('postID');
+    console.log('post detail id = ', selectedPostID );
+
+    
+    
+    this.apiService.getPostDetails( selectedPostID as string ).subscribe({
+      next: ( data => {
+        this.postDetail = data;
+        console.log("post detail = ", this.postDetail )
+      }),
+      error: ( err => console.log('error, cannot find post ', err ))
+    })
+
+
+    this.apiService.getPostComments(selectedPostID as string).subscribe({
+      next: ( data => {
+        this.postComments = data;
+        console.log('comments for selected post', this.postComments)
+      })
+    })
+
+    
+
+  }
 
 }
--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -33,13 +33,53 @@ export class ApiService {
       next: ( data => {
         let updatedPosts = [ data, ...currentPosts ]
         this.AllPostsArray.next( updatedPosts )
+      }
+    ),
+    error: ( err => console.log('error, data not found ', err))
+    })
+  }
+
+
+  getPostDetails( postID: string ): Observable<PostInterface> {
+    return this.httpClient.get<PostInterface>(`https://jsonplaceholder.typicode.com/posts/${ postID }`)
+  }
+
+
+  deletePost( postID: number ) {
+    this.httpClient.delete<PostInterface>(`https://jsonplaceholder.typicode.com/posts/${ postID }`)
+    .subscribe({
+      next: ( data => {
+        console.log("deleted post = ", data )
+        let currentPosts = this.AllPostsArray.getValue()
+        let filteredPosts = currentPosts.filter( post => post.id !== postID );
+        this.AllPostsArray.next( filteredPosts )
       })
     })
   }
 
 
-  getPostDetails( post: PostInterface ) {
-    
+
+  editPost( oldPostID: number, updatedPost: PostInterface ) {
+    let oldPost;
+    this.httpClient.put<PostInterface>(`https://jsonplaceholder.typicode.com/posts/${ oldPostID }`, updatedPost)
+    .subscribe({
+      next: (data => {
+        oldPost = data;
+        console.log("old post = ", oldPost)
+        console.log("new post = ", updatedPost )
+
+        let currentItems = this.AllPostsArray.getValue()
+        let indexOfPostToEdit = currentItems.findIndex( postItem => postItem.id === oldPostID )
+        currentItems[ indexOfPostToEdit ] = updatedPost
+        this.AllPostsArray.next( currentItems )
+      })
+    })
+
+  }
+
+
+  getPostComments(postID: string): Observable<any> {
+    return this.httpClient.get<PostInterface[]>(`https://jsonplaceholder.typicode.com/posts/${ postID }/comments`)
   }
 
 
